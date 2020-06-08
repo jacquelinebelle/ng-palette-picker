1692f4f4cd093b83f96f2fc215494fc0f714d565
Peter Job <peter.job@d55.co.uk>
2020-06-08 17:42:25 +0100

    Update app test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-palette-picker'`, () => {
+  it(`should have as title 'palette picker'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-palette-picker');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng-palette-picker app is running!');
+    expect(app.title).toEqual('palette picker');
   });
 });
